b20009e9213a6ff0142db808e181b7536b8108d3
Kin-afk-ops <linhqwertyuiop2@gmail.com>
2025-07-27 23:29:06 +0700

    update complete v1

--- app/utils/helpers.py
+++ app/utils/helpers.py
@@ -4,16 +4,6 @@ import base64
 from PIL import Image
 import io
 
-# def base64_to_image(base64_str):
-#     img_data = base64.b64decode(base64_str)
-#     np_arr = np.frombuffer(img_data, np.uint8)
-#     img = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
-
-#     # ✅ Tiền xử lý ảnh:
-#     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)        # Chuyển về grayscale
-#     _, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)  # Nhị phân hóa ảnh
-
-#     return thresh  # Trả về ảnh sau xử lý
 
 
 def base64_to_image(base64_str):
--- run.py
+++ run.py
@@ -4,13 +4,18 @@ from pyngrok import ngrok
 from dotenv import load_dotenv
 
 load_dotenv()  #
-
 authtoken = os.getenv('NGROK_TOKEN')
-ngrok.set_auth_token(authtoken)
+ngrok_domain = os.getenv('NGROK_DOMAIN')
+
 
 app = create_app()
 
 if __name__ == "__main__":
-    ngrok_url = ngrok.connect(addr=9999, domain="evident-kingfish-actual.ngrok-free.app")
-    print(ngrok_url)
+    if ngrok_domain:
+        ngrok.set_auth_token(authtoken)
+        ngrok_url = ngrok.connect(addr=9999, domain=ngrok_domain)
+        print(f"Ngrok tunnel: {ngrok_url}")
+    else:
+        print("Running locally on http://localhost:9999")
+
     app.run(port=9999)
